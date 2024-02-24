27ca847466c56f8bd4797cd6d43a6d01347e7fa7
Rangeraluffy <alexandre.rangeraluffy74@gmail.com>
2024-02-24 13:53:45 +0100

    upGrave astro V4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+  },
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "fr", "pt-br", "es"],
+  },
+})
